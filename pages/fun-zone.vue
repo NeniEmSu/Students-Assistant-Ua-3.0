4216c582f9b3d968c3bba75b93d003d7cd6fe90f
cryptodoctor <emmanuelneni@gmail.com>
2019-11-03 22:11:23 +0200

    adding games

---
+++ pages/fun-zone.vue
@@ -0,0 +1,186 @@
+<template>
+  <div>
+    <!-- breadcrumb start-->
+    <!-- <section class="breadcrumb breadcrumb_bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="breadcrumb_iner text-center">
+              <div class="breadcrumb_iner_item">
+                <h2>Games</h2>
+                <p>Home<span>/</span>Fun Zone</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> -->
+    <!-- breadcrumb start-->
+    <section class="blog_part section_padding">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-5">
+            <div class="section_tittle text-center">
+              <p>Fun Zone</p>
+              <h2>Fun and Games</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 col-lg-4 col-xl-4">
+            <div class="single-home-blog">
+              <div class="card">
+                <nuxt-link to="/pericardium-Surgery">
+                  <img
+                    src="~/assets/img/games/pericardium-Surgery.jpg"
+                    class="card-img-top"
+                    alt="pericardium-Surgery"
+                  >
+                </nuxt-link>
+
+                <div class="card-body">
+                  <nuxt-link
+                    to="/pericardium-Surgery"
+                    class="btn_4"
+                  >
+                    Surgery
+                  </nuxt-link>
+                  <nuxt-link to="/pericardium-Surgery">
+                    <h5 class="card-title">
+                      Pericardium Surgery
+                    </h5>
+                  </nuxt-link>
+                  <p>
+                    Learn about the heart, it's pericardium and practice surgery all at the same time.
+                  </p>
+                  <!-- <ul>
+                    <li><span class="ti-comments" />2 Comments</li>
+                    <li><span class="ti-heart" />2k Like</li>
+                  </ul> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-4">
+            <div class="single-home-blog">
+              <div class="card">
+                <nuxt-link to="/shoulder-Surgery">
+                  <img
+                    src="~/assets/img/games/shoulder-Surgery.jpg"
+                    class="card-img-top"
+                    alt="pericardium-Surgery"
+                  >
+                </nuxt-link>
+
+                <div class="card-body">
+                  <nuxt-link
+                    to="/shoulder-Surgery"
+                    class="btn_4"
+                  >
+                    Surgery
+                  </nuxt-link>
+                  <nuxt-link to="/shoulder-Surgery">
+                    <h5 class="card-title">
+                      Shoulder Surgery
+                    </h5>
+                  </nuxt-link>
+                  <p>
+                    Learn about the heart, it's pericardium and practice surgery all at the same time.
+                  </p>
+                  <!-- <ul>
+                    <li><span class="ti-comments" />2 Comments</li>
+                    <li><span class="ti-heart" />2k Like</li>
+                  </ul> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="col-sm-6 col-lg-4 col-xl-4">
+            <div class="single-home-blog">
+              <div class="card">
+                <img
+                  src="~/assets/img/blog/blog_2.png"
+                  class="card-img-top"
+                  alt="blog"
+                >
+                <div class="card-body">
+                  <nuxt-link
+                    to="#"
+                    class="btn_4"
+                  >
+                    Developing
+                  </nuxt-link>
+                  <nuxt-link to="blog.html">
+                    <h5 class="card-title">
+                      All beginning air two likeness
+                    </h5>
+                  </nuxt-link>
+                  <p>
+                    Which whose darkness saying were life unto fish wherein all
+                    fish of together called
+                  </p>
+                  <ul>
+                    <li><span class="ti-comments" />2 Comments</li>
+                    <li><span class="ti-heart" />2k Like</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-4">
+            <div class="single-home-blog">
+              <div class="card">
+                <img
+                  src="~/assets/img/blog/blog_3.png"
+                  class="card-img-top"
+                  alt="blog"
+                >
+                <div class="card-body">
+                  <nuxt-link
+                    to="#"
+                    class="btn_4"
+                  >
+                    Design
+                  </nuxt-link>
+                  <nuxt-link to="blog.html">
+                    <h5 class="card-title">
+                      Form day seasons sea hand
+                    </h5>
+                  </nuxt-link>
+                  <p>
+                    Which whose darkness saying were life unto fish wherein all
+                    fish of together called
+                  </p>
+                  <ul>
+                    <li><span class="ti-comments" />2 Comments</li>
+                    <li><span class="ti-heart" />2k Like</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div> -->
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  scrollToTop: true,
+  components: {
+
+  },
+  meta: {
+    animation: 'fade-in-down'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  border: 0.1px solid gray;
+  object-fit: cover;
+}
+</style>
---
+++ pages/pericardium-Surgery.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="container mt-5 pt-5">
+    <h3 class="mt-5 mb-4 text-center">
+      Pericardium Surgery
+    </h3>
+    <div
+      id="theGame"
+      class="text-center mt-2"
+    >
+      <iframe
+        id="iframegame"
+        seamless
+        scrolling
+        frameborder="0"
+        height="420"
+        width="560"
+        allowfullscreen
+        allow="fullscreen"
+        webkit-playsinline="true"
+        src="//games.cdn.spilcloud.com/1559562554_operate_now_Pericardium_Surgery-https/index.html?gp=1?gp=1&amp;siteid=79&amp;channelid=2&amp;siteLocale=en-US&amp;spilStorageId=87592267524"
+        style="max-width: 560px; width: 100%; height: auto; min-height: 420px;"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import krok2Collection from '~/gql/krok2'
+
+const customLabels = {
+  first: 'First',
+  last: 'Last',
+  previous: '<',
+  next: '>'
+}
+
+export default {
+  apollo: {
+    krok2Collection: {
+      prefetch: true,
+      query: krok2Collection
+    }
+  },
+  filters: {
+    charIndex (i) {
+      return String.fromCharCode(97 + i)
+    }
+  },
+  head: {
+    title: 'Pericardium Surgery'
+  },
+  data () {
+    return {
+      pageOfItems: [],
+      customLabels,
+      pageCount: '1'
+    }
+  },
+
+  methods: {
+    shuffle (a) {
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]]
+      }
+      return a
+    },
+
+    onChangePage (pageOfItems) {
+      // update page of items
+      this.pageOfItems = pageOfItems
+    }
+  }
+}
+</script>
+
+        <style lang="scss" scoped>
+form {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input {
+  margin-bottom: 1rem;
+  width: 20rem;
+  padding: 1.5rem;
+  font-size: 1.8rem;
+  border: none;
+  box-shadow: 0 0.1rem 1.4rem 0 rgba(86, 185, 235, 0.5);
+}
+
+input::placeholder {
+  color: #aaa;
+}
+
+.choice-container {
+  display: flex;
+  margin-bottom: 0.5rem;
+  width: 100%;
+  font-size: 1.8rem;
+  border: 0.1rem solid rgba(5, 13, 20, 0.25);
+  background-color: white;
+}
+
+.choice-container:hover {
+  cursor: pointer;
+  box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
+  transform: translateY(-0.1rem);
+  transition: transform 150ms;
+}
+
+.choice-prefix {
+  padding: 0.5rem 2.5rem;
+  background-color: #56a5eb;
+  color: white;
+}
+
+.choice-text {
+  padding: 0.5rem;
+  width: 100%;
+}
+
+.correct {
+  background-color: #28a745;
+  color: white;
+}
+
+.incorrect {
+  background-color: #b91626;
+  color: white;
+}
+</style>
