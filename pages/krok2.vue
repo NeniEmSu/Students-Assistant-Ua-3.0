5c1650fcc2920fe117b55705ab76aeedd7b71377
cryptodoctor <emmanuelneni@gmail.com>
2019-11-04 20:12:08 +0200

    added the normal values for krok 2

--- pages/krok2.vue
+++ pages/krok2.vue
@@ -94,6 +94,247 @@
       :labels="customLabels"
       @changePage="onChangePage"
     />
+
+    <div class="section-top-border">
+      <h3 class="mb-30">
+        Laboratory values
+      </h3>
+      <h5>Complete blood count</h5>
+      <div class="progress-table-wrap">
+        <div class="progress-table">
+          <div class="table-head">
+            <div class="serial">
+              #
+            </div>
+            <div class="country">
+              Laboratory value
+            </div>
+            <div class="visit">
+              Normal reference range
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              01
+            </div>
+            <div class="country">
+              Erythrocyte count
+            </div>
+            <div class="visit">
+              Male: 4.3 − 5.9 · 1012/L<br>
+              Female: 3.5 − 5.5 · 1012/L
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              02
+            </div>
+            <div class="country">
+              Hemoglobin, blood
+            </div>
+            <div class="visit">
+              Male: 135-175 g/L<br>
+              Female: 120-160 g/L
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              03
+            </div>
+            <div class="country">
+              Color index
+            </div>
+            <div class="visit">
+              0.85 - 1.05
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              04
+            </div>
+            <div class="country">
+              Reticulocyte count
+            </div>
+            <div class="visit">
+              0.5 - 1.5%
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              05
+            </div>
+            <div class="country">
+              Platelet count
+            </div>
+            <div class="visit">
+              150 − 400 · 109/L
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              06
+            </div>
+            <div class="country">
+              Leukocyte count
+            </div>
+            <div class="visit">
+              4.0 − 9.0 · 109/L
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              07
+            </div>
+            <div class="country">
+              Basophils
+            </div>
+            <div class="visit">
+              0 − 0.065 · 109/L (0-1%)
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              08
+            </div>
+            <div class="country">
+              Eosinophils
+            </div>
+            <div class="visit">
+              0.02 − 0.30 · 109/L (0.5 - 5.0%)
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              09
+            </div>
+            <div class="country">
+              Band neutrophils
+            </div>
+            <div class="visit">
+              0.04 − 0.30 · 109/L (1 - 6%)
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              10
+            </div>
+            <div class="country">
+              Segmented neutrophils
+            </div>
+            <div class="visit">
+              2.0 − 5.50 · 109/L (47 - 72%)
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              11
+            </div>
+            <div class="country">
+              Monocytes
+            </div>
+            <div class="visit">
+              0.09 − 0.60 · 109/L (3 - 11%)
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              12
+            </div>
+            <div class="country">
+              Lymphocytes
+            </div>
+            <div class="visit">
+              1.2 − 3.0 · 109/L (19 - 37%)
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              13
+            </div>
+            <div class="country">
+              Erythrocyte sedimentation rate
+              (ESR)
+            </div>
+            <div class="visit">
+              Male: 0 - 15 mm/h <br>
+              Female: 0 - 20 mm/h
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="serial">
+              14
+            </div>
+            <div class="country">
+              Hematocrit
+            </div>
+            <div class="visit">
+              Male: 41 - 53% <br>
+              Female: 36 - 46%
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <h3 class="mb-20">
+      List of abbreviations
+    </h3>
+    <ul class="unordered-list">
+      <li>ACTH => Adrenocorticotrophic hormone</li>
+      <li>ADP => Adenosine diphosphate</li>
+      <li>ALT => ALAT Alanin aminotranspherase</li>
+      <li>ALV => Artificial lung ventilation</li>
+      <li>AP => Arterial (blood) pressure</li>
+      <li>AST => AspAT Aspartat aminotranspherase</li>
+      <li>ATP => Adenosine triphosphate</li>
+      <li>ВСG => Bacillus Calmette-Guérin</li>
+      <li>BP => Blood (arterial) pressure</li>
+      <li>CK => Creatine kinase</li>
+      <li>CNS => Central nervous system</li>
+      <li>СоА => Coenzyme A</li>
+      <li>CRP => C-reactive protein</li>
+      <li>CT => Computed tomography</li>
+      <li>COX => Cyclooxygenase</li>
+      <li>DIC => Disseminated intravascular coagulation</li>
+      <li>DTP => (DPT) Diphtheria-tetanus-pertussis vaccine</li>
+      <li>EDTA => Ethylenediamine tetra-acetic acid</li>
+      <li>ELISA => Enzyme-linked immunosorbent assay</li>
+      <li>ENT => Ear, nose, and throat (as a department in a hospital)</li>
+      <li>EPR => (ER) Endoplasmic reticulum</li>
+      <li>ESR => Erythrocyte sedimentation rate</li>
+      <li>EV => Enterovirus</li>
+      <li>FC => Functional class</li>
+      <li>GABA => Gamma aminobutyric acid</li>
+      <li>Hct => Ht Hematocrit</li>
+      <li>HDL => High-density lipoproteins</li>
+      <li>HR => Heart rate</li>
+      <li>IDL => Intermediate-density lipoproteins</li>
+      <li>IU => International unit</li>
+      <li>IUPAC => International Union of Pure and Applied Chemistry</li>
+      <li>LDH => Lactate dehydrogenase</li>
+      <li>LDL => Low-density lipoproteins</li>
+      <li>LOX => Lipoxygenase</li>
+      <li>MAO => Monoamine oxidase</li>
+      <li>MRI => Magnetic resonance imaging</li>
+      <li>NSAID => Nonsteroidal anti-inflammatory drug</li>
+      <li>PE => (PATE) Pulmonary embolism (Pulmonary artery thromboembolism)</li>
+      <li>PSA => Prostate-specific antigen</li>
+      <li>RBC => Red blood count</li>
+      <li>RR => Respiratory rate</li>
+      <li>SES => Sanitary-and-epidemiologic station</li>
+      <li>STD => Sexually transmitted disease</li>
+      <li>STI => Sexually transmitted infection</li>
+      <li>T/l => Trillion/liter</li>
+      <li>TABT => Typhoid-paratyphoid A and B plus tetanus toxoid vaccine</li>
+      <li>TMJ => Temporomandibular joint</li>
+      <li>TSH => Thyroid-stimulating hormone</li>
+      <li>TU => Tuberculin unit</li>
+      <li>URTI => Upper respiratory tract infection</li>
+      <li>V/f => Vision field</li>
+      <li>VLDL => Very-low-density lipoproteins</li>
+      <li>WBC => White blood count</li>
+      <li>WPW => Wolff-Parkinson-White syndrome</li>
+    </ul>
   </div>
 </template>
 
@@ -142,9 +383,6 @@ export default {
     onChangePage (pageOfItems) {
       // update page of items
       this.pageOfItems = pageOfItems
-      // if (process.client) {
-      //   require('document')
-      // }
       // document.body.scrollTop = document.documentElement.scrollTop = 0
     }
   }
@@ -207,5 +445,17 @@ input::placeholder {
 .incorrect {
   background-color: #b91626;
   color: white;
+}
+
+.progress-table .serial {
+  width: 20%;
+}
+
+.progress-table .country {
+  width: 40%;
+}
+
+.progress-table .visit {
+  width: 40%;
 }
 </style>
