2953f4d6d3164331d031803e3b6aae083a6f46b4
cryptodoctor <emmanuelneni@gmail.com>
2019-11-19 21:58:38 +0200

    added links to semester practice to

---
+++ pages/semesterExam.vue
@@ -0,0 +1,235 @@
+<template>
+  <div class="container my-5 py-5">
+    <div class="landing">
+      <h2>School Exams Selection Page</h2>
+      <p>
+        This section allows you pick Your respective course and subjects enumerated within. pick one and lets
+        get
+        cracking.
+      </p>
+      <p>
+        Feel Free to contact Admin via the <a href="contact">contact form</a> or via the contact information
+        at the <a href="#main-footer"> page end</a> for
+        any enquiries.
+      </p>
+      <h2>How to Use the App</h2>
+      <div class="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/gjN18z796fQ"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen=""
+        />
+      </div>
+      <h2>
+        Medical
+      </h2>
+      <div class="panel">
+        <ul class="subjects-select unordered-list">
+          <!-- <li class="course">
+            <h3>First Course</h3>
+            <ul class="unordered-list">
+              <li><a href="exam">Medical Biology</a></li>
+              <li><a href="#">Latin Language</a></li>
+              <li><a href="#">Medical Physics Diagnostics</a></li>
+              <li><a href="#">Philosophy</a></li>
+            </ul>
+          </li>
+          <li class="course">
+            <h3>Second Course</h3>
+            <ul class="unordered-list">
+              <li><a href="#">Biological and Bioorganic Chemistry</a></li>
+              <li><a href="#">Nursing Practice</a></li>
+              <li><a href="#">Physiology</a></li>
+              <li><a href="#">Ukrainian as a Foreign Language</a></li>
+            </ul>
+          </li>
+          <li class="course">
+            <h3>Second Course Third Semester</h3>
+            <ul class="unordered-list">
+              <li><a href="#">Histology Cytology and Embryology</a></li>
+              <li><a href="#">Human Anatomy</a></li>
+            </ul>
+          </li>
+          <li class="course">
+            <h3>Third Course</h3>
+            <ul class="unordered-list">
+              <li><a href="#">General Surgery</a></li>
+              <li><a href="#">Pathomorphology</a></li>
+              <li><a href="#">Pathophysiology</a></li>
+              <li><a href="#">Phamacology</a></li>
+              <li><a href="#">Propaedeutics of Internal Medicine</a></li>
+              <li><a href="#">Propaedeutics Pediatrics</a></li>
+              <li><a href="#">Radiology</a></li>
+            </ul>
+          </li>
+
+          <li class="course">
+            <h3>Third Course Fifth Semester</h3>
+            <ul class="unordered-list">
+              <li><a href="#">Clinical Anatomy and Operative</a></li>
+              <li><a href="#">Hygiene and Ecology</a></li>
+              <li><a href="#">Microbiology and Immunology</a></li>
+              <li><a href="#">Nursing Practice</a></li>
+            </ul>
+          </li>
+
+          <li class="course">
+            <h3>Fourth Course First Stream</h3>
+            <p>Under Construction! <i class="fas fa-tools" /> </p>
+            <p>Done! <i class="fas fa-check-circle" /></p>
+            <ul class="unordered-list">
+              <li><a href="#">Dermatology and Venerology <i class="fas fa-tools" /> </a></li>
+              <li><a href="#">Internal Medicine (Endocrinology) <i class="fas fa-tools" /> </a></li>
+              <li><a href="#">Neurology <i class="fas fa-tools" /> </a></li>
+              <li><a href="#">Ophthalmology <i class="fas fa-tools" /> </a></li>
+              <li><a href="#">Pediatrics <i class="fas fa-tools" /> </a></li>
+              <li><a href="#">Psyciatry, narcology <i class="fas fa-tools" /> </a></li>
+              <li><a href="#">Surgery <i class="fas fa-tools" /> </a></li>
+              <li><a href="#">Urology <i class="fas fa-tools" /> </a></li>
+            </ul>
+          </li> -->
+
+          <!-- <li class="course">
+            <h3>Fourth Course Second Stream</h3>
+            <p>Under Construction! <i class="fas fa-tools" /> </p>
+            <p>Done! <i class="fas fa-check-circle" /></p>
+            <ul class="unordered-list">
+              <li><a href="#">Internal Medicine (Endocrinology)</a> <i class="fas fa-tools" /> </li>
+              <li><a href="#">Neurology</a></li>
+              <li><a href="https://testpractice.studentsassistantua.com/pages/fourthCourseFirstStream/fourthYearFirstStream_index.php" target="_blank">aryngology</a><i class="fas fa-check-circle" /></li>
+              <li><a href="#">Pediatrics</a> <i class="fas fa-tools" /> </li>
+              <li><a href="https://testpractice.studentsassistantua.com/pages/fourthCourseFirstStream/fourthYearFirstStream_index.php" target="_blank">Phthiology</a><i class="fas fa-check-circle" /></li>
+              <li><a href="https://testpractice.studentsassistantua.com/pages/fourthCourseFirstStream/fourthYearFirstStream_index.php" target="_blank">Physical rehabilitation and sports medicine</a><i class="fas fa-check-circle" /></li>
+              <li><a href="#">Psyciatry, narcology</a> <i class="fas fa-tools" /> </li>
+              <li><a href="#">Surgery</a><i class="fas fa-tools" /></li>
+            </ul>
+          </li> -->
+
+          <li class="course">
+            <h3>Fifth Course First Stream</h3>
+            <p>Under Construction! <i class="fas fa-tools" /> </p>
+            <p>Done! <i class="fas fa-check-circle" /></p>
+            <ul class="unordered-list">
+              <li>
+                <a target="_blank" href="https://drive.google.com/open?id=1tkucYL3kXJDYwTi5QBGpuy0VGJ1GlJbj">Anesthesiology
+                  and intensive care</a><i class="fas fa-check-circle" />
+              </li>
+              <li>
+                <a target="_blank" href="https://drive.google.com/open?id=1hMFgVmFVRmq-ZkSFSBPm8VscP6thOLR2">Epidemiology</a>
+                <i class="fas fa-check-circle" />
+              </li>
+
+              <li>
+                <a href="https://drive.google.com/open?id=1qdKlc2r6XfaADq6MSNXKs2e6bppj5xhG">Traumatology
+                  &amp; Orthopedics</a><i class="fas fa-check-circle" />
+              </li>
+              <li><a href="https://testpractice.studentsassistantua.com/pages/fifthCourseFirstStream/fifthYearFirstStream_index.php" target="_blank">Oncology</a><i class="fas fa-check-circle" /></li>
+              <li><a href="#">Infectious disease</a><i class="fas fa-tools" /></li>
+              <li><a href="#">Internal Medicine</a><i class="fas fa-tools" /></li>
+              <li><a href="#">Obstetrics and Gynecology</a><i class="fas fa-tools" /></li>
+              <li><a href="#">Pediatrics &amp; Pediatrics infectious</a><i class="fas fa-tools" /></li>
+              <li><a href="#">Surgery, Pediatrics surgery </a><i class="fas fa-tools" /></li>
+            </ul>
+          </li>
+          <li class="course">
+            <h3>Fifth Course Second Stream</h3>
+            <p>Under Construction! <i class="fas fa-tools" /> </p>
+            <p>Done! <i class="fas fa-check-circle" /></p>
+            <ul class="unordered-list">
+              <li>
+                <a target="_blank" href="https://drive.google.com/open?id=1tkucYL3kXJDYwTi5QBGpuy0VGJ1GlJbj">Anesthesiology
+                  and intensive care</a><i class="fas fa-check-circle" />
+              </li>
+              <li>
+                <a target="_blank" href="https://drive.google.com/open?id=1hMFgVmFVRmq-ZkSFSBPm8VscP6thOLR2">Epidemiology</a>
+                <i class="fas fa-check-circle" />
+              </li>
+              <li>
+                <a href="https://drive.google.com/open?id=1qdKlc2r6XfaADq6MSNXKs2e6bppj5xhG">Traumatology
+                  &amp; Orthopedics</a><i class="fas fa-check-circle" />
+              </li>
+              <li><a href="#">Infectious disease</a> <i class="fas fa-tools" /></li>
+              <li><a href="#">Internal Medicine</a> <i class="fas fa-tools" /></li>
+              <li><a href="#">Obstetrics and Gynecology</a> <i class="fas fa-tools" /></li>
+              <li>
+                <a href="#">Pediatrics &amp; Pediatrics infectious</a> <i class="fas fa-tools" />
+              </li>
+              <li><a href="#">Surgery, Pediatrics surgery </a><i class="fas fa-tools" /></li>
+            </ul>
+          </li>
+          <li class="course">
+            <h3>Sixth Course First Stream</h3>
+            <p>Under Construction! <i class="fas fa-tools" /> </p>
+            <p>Done! <i class="fas fa-check-circle" /></p>
+            <ul class="unordered-list">
+              <li><a href="https://drive.google.com/drive/folders/1HOBDmLE6NCfSl32XQlL7qfKQRh73qBJF?usp=sharing" target="_blank"> Download Internal Medicine Practice App</a><i class="fas fa-check-circle" /></li>
+              <li><a href="https://drive.google.com/drive/folders/18BToCBsUpYix45S0yeiFSwvsWNimrmXp?usp=sharing" target="_blank"> Download Obstetrics and gynecology Practice App</a><i class="fas fa-check-circle" /></li>
+            </ul>
+          </li>
+
+          <li class="course">
+            <h3>Sixth Course Second Stream</h3>
+            <p>Under Construction! <i class="fas fa-tools" /> </p>
+            <p>Done! <i class="fas fa-check-circle" /></p>
+            <ul class="unordered-list">
+              <li><a href="#">General Practice (Family medicine)</a><i class="fas fa-tools" /></li>
+
+              <li><a href="https://drive.google.com/drive/folders/11NYAzeSgjlyaUBzg854OzfqxGv0Pv7Df?usp=sharing" target="_blank"> Download Hygiene and ecology Practice App</a><i class="fas fa-check-circle" /></li>
+              <li>
+                <a href="https://drive.google.com/drive/folders/1OJbL6HmxC4u4sO8dPqUCVh4ClrgoKzUC?usp=sharing" target="_blank"> Download Pediatrics with children_s infectious diseases
+                  Practice App</a><i class="fas fa-check-circle" />
+              </li>
+              <li>
+                <a href="https://drive.google.com/drive/folders/1-Qys5tbmfLHmjkj8D22rD-HbLX0MlEwQ?usp=sharing" target="_blank"> Download Social medicine, organization and economy of health
+                  protection Practice App</a><i class="fas fa-check-circle" />
+              </li>
+              <li>
+                <a href="https://drive.google.com/drive/folders/1_dpeHBhJynwv81NR7-msZmHvgIPvZgkc?usp=sharing" target="_blank"> Download Surgery with children_s surgery and oncology Practice
+                  App</a><i class="fas fa-check-circle" />
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <h2 class="accordion nursing active">
+        Nursing
+      </h2>
+      <div class="panel" style="max-height: 348px;">
+        <h3>BSN 1st Course</h3>
+        <p>
+          <strong>After Clicking On Link, Download and Extract Folder onto Device </strong> <br>
+          This folder contains practice for Anatomy, Physiology, Phsycology and Medical Biology.
+        </p>
+        <a href="https://drive.google.com/open?id=1d7x2CK5bSIWvMJbobhuXUB5LGxRjgQHR" target="_blank">
+          Download Practice App</a><i class="fas fa-check-circle" />
+        <h3>BSN 2nd Course  1st semester</h3>
+        <p>
+          <strong>After Clicking On Link, Download and Extract Folder onto Device </strong> <br>
+          This folder contains practice for Biochemistry, Leadership in nursing, Microbiology, Nutrition
+          and Phamacology.
+        </p>
+        <a href="https://drive.google.com/open?id=1S3NXh80JszhTZVN9i3-9K8_yacCdJN2t" target="_blank">
+          Download Practice App</a><i class="fas fa-check-circle" />
+        <h3>BSN 2nd Course 2st semester</h3>
+        <p>
+          <strong>After Clicking On Link, Download and Extract Folder onto Device </strong> <br>
+          This folder contains practice for Biochemistry, Leadership in nursing, Microbiology, Nutrition
+          and Phamacology.
+        </p>
+        <a href="https://drive.google.com/open?id=1_0aLeqs36PvuPMF-f10LuCu9__dJm-xD" target="_blank">
+          Download Practice App</a><i class="fas fa-check-circle" />
+        <h3>RN-BSN 1st Course</h3>
+        <p>
+          <strong>After Clicking On Link, Download and Extract Folder onto Device </strong> <br>
+          This folder contains practice for .
+        </p>
+        <a href="https://drive.google.com/open?id=1bLvA3dNNvKqLM7_JbfHd5Eh1l_8Yc4by" target="_blank">
+          Download Practice App</a><i class="fas fa-check-circle" />
+      </div>
+    </div>
+  </div>
+</template>
